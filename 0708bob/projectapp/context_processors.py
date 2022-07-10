bad8c7614187e5fbb70099abf1e29e342dfbe476
Oh minyoung <46808752+ohMinyoung22@users.noreply.github.com>
2022-07-10 09:45:01 +0900

    first commit

---
+++ 0708bob/projectapp/context_processors.py
@@ -0,0 +1,15 @@
+from accounts.models import User
+
+def login_processor(request):
+    context = {}
+
+    login_session = request.session.get('login_session', '')
+
+    if login_session == '':
+        context['login_session'] = False
+        context['username'] = '익명'
+    else:
+        context['login_session'] = True
+        context['username'] = User.objects.get(username=login_session)
+
+    return context
